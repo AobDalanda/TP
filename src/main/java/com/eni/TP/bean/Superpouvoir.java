72579a39a9ba07455c07628f94579004ba121192
bsidy <bsidy01@gmail.com>
2021-08-23 10:17:55 +0200

    first commit2

--- src/main/java/com/eni/TP/bean/Superpouvoir.java
+++ src/main/java/com/eni/TP/bean/Superpouvoir.java
@@ -9,9 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Superpouvoir {
 
@@ -22,4 +19,55 @@ public class Superpouvoir {
     private String description;
     private int maxscore;
 
+    public Superpouvoir() {
+    }
+
+
+    public Superpouvoir(String name, String description, int maxscore) {
+        this.name = name;
+        this.description = description;
+        this.maxscore = maxscore;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMaxscore() {
+        return maxscore;
+    }
+
+    public void setMaxscore(int maxscore) {
+        this.maxscore = maxscore;
+    }
+
+    @Override
+    public String toString() {
+        return "Superpouvoir{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", maxscore=" + maxscore +
+                '}';
+    }
 }
